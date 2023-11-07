cc1772c57616e9cffde518c3546e283e453d93be
leonardchinonso <36096513+leonardchinonso@users.noreply.github.com>
2023-11-07 22:34:45 +0000

    jokes

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     // TODO: log the arguments to the console
     println!("{:?}", args);
 
-    let mut file = File::new(args.file_name().as_str(), "out_data.txt");
-    let text = file.read().expect("cannot read file!");
+    let mut pedo_file = File::new(args.file_name().as_str(), "out_data.txt");
+    let text = pedo_file.read().expect("cannot read file!");
 
     let mut codec = io::new_codec(text, args.algorithm())?;
     codec.encode();
-    file.write(codec.compressed().as_bytes())
+    pedo_file.write(codec.compressed().as_bytes())
         .expect("cannot write codec to file!");
     codec.decode();
-    file.write(codec.decompressed().as_bytes())
+    pedo_file.write(codec.decompressed().as_bytes())
         .expect("cannot write output to file!");
 
     // let mut rle = RLE::new(text);
@@ -43,7 +43,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // read output file and compare the contents
     let mut file2 = File::new("out_data.txt", "");
     file2.read().expect("cannot read output file!");
-    assert_eq!(file, file2);
+    assert_eq!(pedo_file, file2);
 
     Ok(())
 }
