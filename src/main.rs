f95a32bf8b2a3cc2c2817c14a878f91d2aa2ff61
leonardchinonso <36096513+leonardchinonso@users.noreply.github.com>
2023-11-29 02:32:44 +0000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -26,20 +26,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     file.write(codec.decompressed().as_bytes())
         .expect("cannot write output to file!");
 
-    // let mut rle = RLE::new(text);
-    // rle.encode();
-    // file.write(rle.compressed().as_bytes()).expect("cannot write codec to file!");
-    // rle.decode();
-    // file.write(rle.decompressed().as_bytes()).expect("cannot write output to file!");
-
-    // let mut huffman = Huffman::new(text);
-    // huffman.encode();
-    // file.write(huffman.compressed().as_bytes())
-    //     .expect("cannot write codec to file!");
-    // huffman.decode();
-    // file.write(huffman.decompressed().as_bytes())
-    //     .expect("cannot write output to file!");
-    //
     // read output file and compare the contents
     let mut file2 = File::new("test_data/out_data.txt", "");
     file2.read().expect("cannot read output file!");
